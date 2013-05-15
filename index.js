d702002a4bd8f6f6c72b8e9d6d0edc81746fc093
Lance Pollard <lancejpollard@gmail.com>
2013-05-14 20:24:27 -0700

    emitting

--- index.js
+++ index.js
@@ -3,15 +3,104 @@
  * Module dependencies.
  */
 
-var indexof = require('indexof')
+var Emitter = require('tower-emitter')
+  , indexof = require('indexof')
   , isArray = require('part-is-array');
 
 /**
  * Expose `collection`.
  */
 
-module.exports = collection;
+exports = module.exports = collection;
 
-function collection() {
-  
+/**
+ * Expose `Collection`.
+ */
+
+exports.Collection = Collection;
+
+/**
+ * Instantiate a new `Collection`.
+ */
+
+function collection(array) {
+  return new Collection(array);
 }
+
+/**
+ * Instantiate a new `Collection`.
+ */
+
+function Collection(array) {
+  this.array = array || [];
+  this.length = this.array.length;
+}
+
+/**
+ * Mixin `Emitter`.
+ */
+
+Emitter(Collection.prototype);
+
+Collection.prototype.push = function(){
+  var startIndex = this.array.length;
+  this.apply('push', arguments);
+  this.length = this.array.length;
+  if (this.hasListeners('add'))
+    this.emit('add', this.array.slice(startIndex, this.length), startIndex);
+  return this;
+};
+
+Collection.prototype.pop = function(){
+  this.apply('pop', arguments);
+  this.length = this.array.length;
+};
+
+Collection.prototype.shift = function(){
+  this.apply('shift', arguments);
+  this.length = this.array.length;
+};
+
+Collection.prototype.unshift = function(){
+  this.apply('unshift', arguments);
+  this.length = this.array.length;
+};
+
+Collection.prototype.splice = function(index, length, item){
+  this.apply('splice', arguments);
+  this.length = this.array.length;
+};
+
+Collection.prototype.slice = function(index, length, item){
+  var result = this.apply('slice', arguments);
+  this.length = this.array.length;
+  return result;
+};
+
+Collection.prototype.forEach = function(fn, binding){
+  this.apply('forEach', arguments);
+};
+
+Collection.prototype.indexOf = function(){
+  this.apply('indexOf', arguments);
+};
+
+Collection.prototype.reverse = function(){
+  this.apply('reverse', arguments);
+};
+
+Collection.prototype.sort = function(){
+  this.apply('sort', arguments);
+};
+
+Collection.prototype.toArray = function(){
+  return this.array;
+};
+
+/**
+ * @api private
+ */
+
+Collection.prototype.apply = function(method, args){
+  this.array[method].apply(this.array, args);
+};
--- test/index.js
+++ test/index.js
@@ -5,7 +5,52 @@ var collection = 'undefined' == typeof window
 var assert = require('assert');
 
 describe('collection', function(){
-  it('should test', function(){
-    assert.equal(1 + 1, 2);
+  it('should take an array', function(){
+    var array = [1, 2, 3];
+    var items = collection(array);
+    assert(array === items.toArray());
+  });
+
+  it('should create empty array if no args passed', function(){
+    var items = collection();
+    assert(0 === items.toArray().length);
+  });
+
+  it('#push', function(){
+    var items = collection();
+    items.push(1);
+    assert('1' === items.toArray().join(','));
+    assert(1 === items.length);
+
+    var items = collection();
+    items.push(1, 2, 3);
+    assert('1,2,3' === items.toArray().join(','));
+    assert(3 === items.length);
+
+    var items = collection();
+    items.push([1, 2, 3]);
+    assert('1,2,3' === items.toArray().join(','));
+    assert(1 === items.length);
+  });
+
+  describe('events', function(){
+    it('#push', function(){
+      var calls = [];
+      var items = collection();
+      items
+        .on('add', function(items, startIndex){
+          calls.push({ items: items, i: startIndex });
+        });
+      items.push(1);
+      items.push(1, 2, 3);
+      items.push([1, 2, 3]);
+      assert(3 === calls.length);
+      assert(0 === calls[0].i);
+      assert('1' === calls[0].items.join(','));
+      assert(1 === calls[1].i);
+      assert('1,2,3' === calls[1].items.join(','));
+      assert(4 === calls[2].i);
+      assert('1,2,3' === calls[2].items.join(','));
+    })
   });
 });
